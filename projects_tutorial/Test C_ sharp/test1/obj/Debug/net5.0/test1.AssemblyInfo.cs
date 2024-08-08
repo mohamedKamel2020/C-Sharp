d8a809bce70d0e5e265dc0c232de2cded0951343
mohamedKamel2020 <m.kamelmohamed2020@gmail.com>
2024-08-08 08:19:50 +0300

    edit

--- projects_tutorial/Test C_ sharp/test1/obj/Debug/net5.0/test1.AssemblyInfo.cs
+++ projects_tutorial/Test C_ sharp/test1/obj/Debug/net5.0/test1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc937fba9bafad1e1370d153364de359b5c6f48")]
 [assembly: System.Reflection.AssemblyProductAttribute("test1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
