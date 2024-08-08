4868cb30e497e7058223faf0e580d4813ade4f95
mohamedKamel2020 <m.kamelmohamed2020@gmail.com>
2024-08-08 08:19:39 +0300

    edit

--- C#_Beginners_Course/FileStreamC_Sharp/FileStreamC_Sharp/obj/Debug/net6.0/FileStreamC_Sharp.AssemblyInfo.cs
+++ C#_Beginners_Course/FileStreamC_Sharp/FileStreamC_Sharp/obj/Debug/net6.0/FileStreamC_Sharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileStreamC_Sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc937fba9bafad1e1370d153364de359b5c6f48")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileStreamC_Sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileStreamC_Sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
